87584d1902d2fdb1890f4e8637765d46a844440f
Rasmus Vraa Bilgram <Rasmus Vraa Bilgram>
2016-11-15 14:03:40 +0100

    Start på ranking system

---
+++ Rankingsystem/Rankingsystem/Classes/Roles/Bot.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rankingsystem.Classes.Roles
+{
+    class Bot
+    {
+        //Bot features
+        private float laneMinion;
+        private int minionDiff;
+        private int dmgToChamps;
+
+        public int DmgToChamps
+        {
+            get { return dmgToChamps; }
+            set { dmgToChamps = value; }
+        }
+
+        public int MinionDiff
+        {
+            get { return minionDiff; }
+            set { minionDiff = value; }
+        }
+
+        public float LaneMinions
+        {
+            get { return laneMinion; }
+            set { laneMinion = value; }
+        }
+
+    }
+}
---
+++ Rankingsystem/Rankingsystem/Classes/Roles/Support.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rankingsystem.Classes.Roles
+{
+    class Support
+    {
+        //Support
+        private int assists;
+
+        public int Assists
+        {
+            get { return assists; }
+            set { assists = value; }
+        }
+
+    }
+}
