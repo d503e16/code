dea63ab9937167b8b61bbfce3f1ad48843fde976
Rasmus Vraa Bilgram <Rasmus Vraa Bilgram>
2016-11-28 14:14:46 +0100

    startet på neural network

---
+++ Rankingsystem/Classes/NeuralNetwork/Rank.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Encog.Engine.Network.Activation;
+using Encog.ML.Data;
+using Encog.ML.Data.Basic;
+using Encog.ML.Train;
+using Encog.Neural.Networks;
+using Encog.Neural.Networks.Layers;
+using Encog.Neural.Networks.Training.Propagation.Back;
+using Encog.Neural.Networks.Training.Propagation.Resilient;
+
+
+namespace Rankingsystem.Classes.NeuralNetwork
+{
+    class Rank
+    {
+        public static double[][] input =
+        {
+            new[] {5.5, 3.5},
+            new[] {3.2, 5.1},
+        };
+
+        public static double[][] ideal =
+        {
+            new[] {0.0},
+            new[] {1.0},
+        };
+
+        public void execute()
+        {
+            var network = new BasicNetwork();
+            network.AddLayer(new BasicLayer(new ActivationLinear(), true, 2));
+            network.AddLayer(new BasicLayer(new ActivationLinear(), true, 2));
+            network.AddLayer(new BasicLayer(new ActivationSigmoid(), true, 1));
+            network.Structure.FinalizeStructure();
+            network.Reset();
+            
+            IMLDataSet trainingSet = new BasicMLDataSet(input, ideal);
+
+            IMLTrain train = new Backpropagation(network, trainingSet);
+
+            int epoch = 1;
+
+            do
+            {
+                train.Iteration();
+                Console.WriteLine(@"Epoch #" + epoch + @" Error:" + train.Error);
+                epoch++;
+            } while (train.Error > 0.01);
+
+            Console.WriteLine(@"Neural Network Results:");
+            foreach (IMLDataPair pair in trainingSet)
+            {
+                IMLData output = network.Compute(pair.Input);
+                Console.WriteLine(pair.Input[0] + @"," + pair.Input[1]
+                                  + @", actual=" + output[0] + @",ideal=" + pair.Ideal[0]);
+            }
+        }
+    }
+}
--- Rankingsystem/Program.cs
+++ Rankingsystem/Program.cs
@@ -7,6 +7,7 @@ using Rankingsystem.Classes;
 using Newtonsoft.Json;
 using System.Net.Http;
 using Rankingsystem.Classes.Roles;
+using Rankingsystem.Classes.NeuralNetwork;
 
 namespace Rankingsystem
 {
@@ -15,19 +16,21 @@ namespace Rankingsystem
     {
         static void Main(string[] args)
         {
-            Database database = new Database();
-            database.InitDatabase();
-            HttpClient client = new HttpClient();
-            var response = client.GetStringAsync("https://euw.api.pvp.net/api/lol/euw/v2.2/match/2776285553?api_key=RGAPI-3b849016-e4b4-4e6e-bf2d-c5e74e2368a5");
-            MatchAPI match = JsonConvert.DeserializeObject<MatchAPI>(response.Result);
-            List<Participant> a = new List<Participant>();
-            for (int i = 0; i < 10; i++)
-            {
-                a.Add(match.getparticipant(i));
-            }
-            
-            var summoner = database.GetSummoner(1);
-            Console.WriteLine("test");
+            //Database database = new Database();
+            //database.InitDatabase();
+            //HttpClient client = new HttpClient();
+            //var response = client.GetStringAsync("https://euw.api.pvp.net/api/lol/euw/v2.2/match/2776285553?api_key=RGAPI-3b849016-e4b4-4e6e-bf2d-c5e74e2368a5");
+            //MatchAPI match = JsonConvert.DeserializeObject<MatchAPI>(response.Result);
+            //List<Participant> a = new List<Participant>();
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    a.Add(match.getparticipant(i));
+            //}
+
+            //var summoner = database.GetSummoner(1);
+            //Console.WriteLine("test");
+            Rank r = new Rank();
+            r.execute();
             Console.ReadKey();
         }
     }
