5a03bb48df54299d0b71ac7a93e3697ea9402dff
rasmuskrusaa <rasmuskrusaa@gmail.com>
2016-12-08 11:53:38 +0100

    Testing done

---
+++ RankingSystemTest/DatabaseTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RankingSystemTest
+{
+    [TestClass]
+    public class DatabaseTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
